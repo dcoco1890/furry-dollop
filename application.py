576f3cfe16c4277075f6bb049a9978b53ab98253
Cohen <dcoco91@gmail.com>
2020-06-02 16:10:25 -0400

    add main application.py plus get ready for templates with jinja

---
+++ application.py
@@ -0,0 +1,19 @@
+import os
+
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
