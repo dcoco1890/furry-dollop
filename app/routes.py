62ae5a99f72569e6fb13e6442661bc67b08646ec
Cohen <dcoco91@gmail.com>
2020-06-05 14:37:15 -0400

    added login route and login page (nothing really there yet though) added a bootstrap collapsable navbar, changed some config options

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,8 @@ def home():
 
 @app.route("/add", methods=['GET'])
 def create_user():
+    # Most of this stuff is just testing to make sure things work, will remove
+    #TODO make this better
     un = request.args.get('user')
     p = request.args.get('pass')
     session['test'] = 'yes'
@@ -22,3 +24,17 @@ def create_user():
         db.session.commit()
     users = User.query.all()
     return render_template("index.html", users=users)
+
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+
+    # commenting out for now as session is not really set up yet
+    # session.clear
+
+    if request.method == 'POST':
+        # TODO verify user has entered information in the login form, validate
+        # user exists in the db, checkpasshash, redirect to homepage
+    else:
+        # GET request on /login takes users to login form
+        return render_template('login.html')
