5616ce2528342efcb011583d1cf52915a432bcdd
Cohen <dcoco91@gmail.com>
2020-07-23 10:50:47 -0400

    added documentation for my strip function (strip_fix) that modifies the API data into just what I want. Added a function that constructs links to the audio files for me based on MW's documentation

--- app/helpfunc.py
+++ app/helpfunc.py
@@ -40,23 +40,39 @@ def lookup_word(word):
 def audio_url(aud_list):
     nlist = []
     for sound in aud_list:
-        x = sound['sound']['audio'][0]
-        if x.isdigit() or x in string.punctuation:
-            sub = "number"
-        elif x == "b":
-            sub = "bix"
-        elif x == "g":
-            sub = "gg"
-        else:
-            sub = x
-        aud_url = f"https://media.merriam-webster.com/audio/prons/en/us/mp3/{sub}/{sound['sound']['audio']}.mp3"
-        nlist.append(aud_url)
+        # Not all items in list have the sound key, wrapped in try catch to fix this
+        try:
+            x = sound['sound']['audio'][0]
+            if x.isdigit() or x in string.punctuation:
+                sub = "number"
+            elif x == "b":
+                sub = "bix"
+            elif x == "g":
+                sub = "gg"
+            else:
+                sub = x
+            aud_url = f"https://media.merriam-webster.com/audio/prons/en/us/mp3/{sub}/{sound['sound']['audio']}.mp3"
+            nlist.append(aud_url)
+        except KeyError:
+            pass
     return nlist
 
-#TODO handle edge cases: alternate head words
+
+
 # Not finished but
+# ------------
 # strip_fix takes in the list returned from API call and converts it into something a little easier for me to use
 # with the application. Should hopefully keep the routes.py & lookup.html from doing to much parsing
+# Each object has the following properties:
+    # shortdef, a short definition (some words don't have one so it ends up an empty list)
+    # head, one or more "headwords" which are similar words or more words to give context
+    # stems, a list of stem words, usually pluralized or conjugated versions of headword
+# Optionally, each word may have 0 or more of the following:
+    # audio, a link to a pronunciation of the word
+    # quotes, a list of one or more objs with: quote itself, year, publication, author
+# ------------
+
+
 def strip_fix(list_words=None):
 
     if list_words is None:
@@ -68,7 +84,9 @@ def strip_fix(list_words=None):
             new_item = {"shortdef": item["shortdef"], "head": item["hwi"]["hw"], "stems": item["meta"]["stems"]}
             try:
                 new_item["audio"] = audio_url(item["hwi"]["prs"])
-            except KeyError:
+                new_item["quotes"] = item["quotes"]
+            except KeyError as e:
+                print(f"the key [{e}] was not found for this entry")
                 pass
             fixed_list.append(new_item)
 
